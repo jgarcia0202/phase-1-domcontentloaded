87c6295d68dd30f32f39ce061ed7ff20b9695a12
Jose <jmgarcia0202@gmail.com>
2022-06-08 15:59:54 -0700

    redid assignment

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', e => {
+  let text = document.querySelector('#text')
+  text.textContent = 'This is really cool!'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let text = document.getElementById('text');
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("The DOM has loaded");
     text.textContent = "This is really cool!";
   });
+*/
